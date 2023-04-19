6c8be0d9673ba924ee480bc0d419bdd0308816a4
Solu04 <solutionresource15@gmail.com>
2023-04-19 07:46:22 -0700

    [Working]: Modifying execute function to handle cmd arguments

--- execute.c
+++ execute.c
@@ -1,32 +1,33 @@
 #include "main.h"
+
 /**
- * execute - executes the given command
- * @command: the command to execute
- * Retrun: Void
+ * execute - executes the given command with its arguments
+ * @args: array of strings containing the command and its arguments
+ * Return: void
  */
-void execute(char *command)
+void execute(char **args)
 {
-        pid_t pid;
-        int status;
+    pid_t pid;
+    int status;
 
-        pid = fork();
+    pid = fork();
 
-        if (pid == 0)
-                {
-                        if (execlp(command, command, (char *)0) == -1)
-                        {
-                                perror("./shell");
-                        }
-                        exit(EXIT_FAILURE);
-                }
-        else if (pid < 0)
-                {
-                perror("simple_shell");
-        }
-        else
+    if (pid == 0)
+    {
+        if (execvp(args[0], args) == -1)
         {
-                do {
-                        waitpid(pid, &status, WUNTRACED);
-                } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+            perror("simple_shell");
         }
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+        perror("simple_shell");
+    }
+    else
+    {
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
 }
