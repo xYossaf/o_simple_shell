e27492b72a17b7d70c11abfdb7f715d9922f22c5
Solu04 <solutionresource15@gmail.com>
2023-04-28 16:52:53 -0700

    [Working]: Fix error in execute function

--- execute.c
+++ execute.c
@@ -1,145 +1,31 @@
 #include "main.h"
 
 /**
- * execute - executes the given command with its arguments
- * @args: array of strings containing the command and its arguments
- * Return: void
+ * execute - execute the commands
+ * @parse: is a char array of pointers
+ * Return: Always 0.
  */
 
-void execute(char **args)
+int execute(char **parse)
 {
-	char *path;
-	char **paths;
-	char *cmd;
-/* Check if the command is "exit" */
-if (strcmp(args[0], "exit") == 0)
-{
-exit(0);
-}
+	pid_t pid;
+	int status;
 
-/* Check if the command is "env" */
-if (strcmp(args[0], "env") == 0)
-{
-print_env();
-return;
-}
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(parse[0], parse, NULL) == -1)
+		{
+			perror(parse[0]);
+			exit(1);
+		}
+	}
+	else if (pid > 0)
+	{
+		wait(&status);
+	}
+	else
+		perror("Error:");
 
-/* Check if the command exists in one of the directories in PATH */
-path = getenv("PATH");
-paths = split_path(path);
-cmd = find_command(args[0], paths);
-free_split(paths);
-
-if (cmd != NULL)
-{
-/* Command exists, fork and execute */
-fork_and_execute(args, cmd);
-free(cmd);
-}
-else
-{
-/* Command not found */
-fprintf(stderr, "%s: command not found\n", args[0]);
-}
-}
-
-/**
- * print_env - prints env
- *
- * Return: void
- */
-
-void print_env(void)
-{
-char **env = __environ;
-while (*env)
-{
-printf("%s\n", *env);
-env++;
-}
-}
-/**
- * split_path - splits path
- * @path: path given
- * Return: char - path spilt
- */
-char **split_path(char *path)
-{
-char **paths = malloc(sizeof(char *));
-char *p = strtok(path, ":");
-
-int i = 0;
-while (p != NULL)
-{
-paths[i] = p;
-i++;
-
-paths = realloc(paths, (i + 1) * sizeof(char *));
-p = strtok(NULL, ":");
-}
-
-paths[i] = NULL;
-return (paths);
-}
-/**
- * find_command - splits path
- * @cmd: cmd given
- * @paths: paths given
- * Return: char -  full path
- */
-char *find_command(char *cmd, char **paths)
-{
-char *path = NULL;
-int i;
-
-for (i = 0; paths[i] != NULL; i++)
-{
-char *full_path = malloc(strlen(paths[i]) + strlen(cmd) + 2);
-sprintf(full_path, "%s/%s", paths[i], cmd);
-
-if (access(full_path, X_OK) == 0)
-{
-/* Command exists */
-path = full_path;
-break;
-}
-
-free(full_path);
-}
-
-return (path);
-}
-
-/**
- * fork_and_execute - splits path
- * @args: args given
- * @cmd: cmd given
- * Return: void
- */
-void fork_and_execute(char **args, char *cmd)
-{
-pid_t pid = fork();
-int status;
-
-if (pid == 0)
-{
-/* Child process */
-if (execvp(cmd, args) == -1)
-{
-perror("simple_shell");
-}
-
-exit(EXIT_FAILURE);
-}
-else if (pid < 0)
-{
-perror("simple_shell");
-}
-else
-{
-/* Parent process */
-do {
-waitpid(pid, &status, WUNTRACED);
-} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-}
+	return (0);
 }
